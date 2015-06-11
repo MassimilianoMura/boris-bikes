1a892f45ca3efd220d3ca859e0652eb6e8343115
Robert Morgan <robbie.morgan@gmail.com>
2015-06-11 16:18:55 +0100

    Not releasing broken bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
 
@@ -14,10 +15,18 @@ describe DockingStation do
   it {is_expected.to respond_to(:dock).with(1).argument}
 
   describe 'release_bike' do
+
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
+    
+  it "Doesn't release a broken bike" do 
+    subject.dock Bike.new.report_broken
+    expect { subject.release_bike }.to raise_error 'No bikes available'#means bike is broken
+    # expect(bike).to be_working
+    end
   end
+
 
   describe 'dock' do
     it 'raises an error when is full' do
@@ -29,6 +38,10 @@ describe DockingStation do
   it 'has a default capacity' do
   	expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
+
+
+
+
 
   # it {is_expected.to respond_to(:increase_capacity).with(1).argument}
 

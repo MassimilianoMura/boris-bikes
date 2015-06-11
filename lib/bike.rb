96170e1cf941cc6f5f502f725d51871bf7baa42c
Robert Morgan <robbie.morgan@gmail.com>
2015-06-11 14:28:26 +0100

    Introduced broken bikes

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,7 +1,19 @@
-class Bike
+class Bike 
 
-  def working?
-    true
-  end
+def initialize
+  @working=true
+end
+
+def broken?
+	!@working
+end
+  
+def working?
+  @working
+end
+
+def report_broken
+	@working = false
+end
 
 end
